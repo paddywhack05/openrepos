edbab30d999fc77d5ac9dababcbc5ab55082e2de
paddywhack05 <100291163+paddywhack05@users.noreply.github.com>
2022-03-12 00:45:48 +0000

    Delete ringslash.js

--- ringslash.js
+++
@@ -1,9 +0,0 @@
-
-
-module.exports= {
-    name: "ringslash",
-    description:"ringslash command",
-    execute(interaction){
-        interaction.reply ({content: "https://www.youtube.com/watch?v=Vk4KK-gh0FM"})
-    }
-}
