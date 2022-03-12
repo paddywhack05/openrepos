42f638b78d77e46afe7b5b09d2823190a28e56b6
paddywhack05 <100291163+paddywhack05@users.noreply.github.com>
2022-03-12 00:44:36 +0000

    Delete hibot.js

--- hibot.js
+++
@@ -1,9 +0,0 @@
-module.exports= {
-    name: "hibot",
-    description:"hibot command",
-    execute(message){
-        message.reply({
-            content: 'hello '+ message.author.username,
-        })
-    }
-}
