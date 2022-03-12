2021877860a1c5115fd1e7401ee0603e7374e463
paddywhack05 <100291163+paddywhack05@users.noreply.github.com>
2022-03-12 00:45:41 +0000

    Delete ring.js

--- ring.js
+++
@@ -1,7 +0,0 @@
-module.exports= {
-    name: "ring",
-    description:"ring command",
-    execute(message){
-        message.reply ({content: "https://www.youtube.com/watch?v=Vk4KK-gh0FM"})
-    }
-}
